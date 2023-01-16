e3443b498acf7aab0346326c4c768d414d7aefb6
MartinLucena28 <martinlucena02@gmail.com>
2023-01-15 21:17:10 -0300

    1.1

---
+++ src/main/java/com/portfolio/cml/CmlApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/cml/CmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.cml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
